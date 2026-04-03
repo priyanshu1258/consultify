e97faebcfd97f220855c1877201222a7ff7d0096
Priyanshu Attri <priyanshuattri05@gmail.com>
2026-04-04 01:23:43 +0530

    added the url or backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create an Axios instance that defaults to the environment variable VITE_API_URL
 // If the variable is not set (during local dev), it falls back to http://localhost:5000
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://consluteebackend.onrender.com",
 });
 
 // You can also add interceptors here later if you want to attach JWT tokens to every request automatically
-api.interceptors.request.use((config) => {
-  const userInfo = localStorage.getItem('userInfo');
-  if (userInfo) {
-    const { token } = JSON.parse(userInfo);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo) {
+      const { token } = JSON.parse(userInfo);
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
