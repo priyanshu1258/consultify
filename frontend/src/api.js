7e7c7f30a2e983ba7dc6cc94d5ec85fa744855ec
Priyanshu Attri <priyanshuattri05@gmail.com>
2026-04-05 01:44:57 +0530

    it will pick the server link through env only

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 // Create an Axios instance that defaults to the environment variable VITE_API_URL
-// If the variable is not set (during local dev), it falls back to http://localhost:5000
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // You can also add interceptors here later if you want to attach JWT tokens to every request automatically
